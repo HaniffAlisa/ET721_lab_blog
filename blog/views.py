ec3838a67c7c4e1313b7e96a3c43e405595a15a7
HaniffAlisa <154397440+HaniffAlisa@users.noreply.github.com>
2024-11-15 18:41:00 +0000

    step 3 - blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'post_list.html')
+from django.shortcuts import render, get_object_or_404
+from .models import BlogPost
+
+def post_list(request):
+    posts = BlogPost.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(BlogPost, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_create(request):
+    # Form logic for creating a post
+    pass
+
+def post_edit(request, pk):
+    # Form logic for editing a post
+    pass
+    
